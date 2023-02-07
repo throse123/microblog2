3dfd48d7496139bcc8a8ac1c240b753ffdb41ddb
throse123 <throse@packer.edu>
2023-02-06 20:08:23 -0500

    update

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,12 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+@app.route('/contact')
+@app.route('/cool_poem')
 def index():
     return "Hello, World!"
+def contact():
+    return "My email is throse@packer.edu"
+def cool_poem():
+    return "The red tiger was in its cage. Yes yes. the tiger it out."
+
