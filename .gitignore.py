e4e1481a87254258fab5fb8919c120c1f9ad2133
throse123 <throse@packer.edu>
2023-02-09 10:52:09 -0500

    changes

---
+++ .gitignore.py
@@ -0,0 +1,41 @@
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> *.py[cod]
+... 
+... # C extensions
+... *.so
+... 
+... # Packages
+... *.egg
+... *.egg-info
+... dist
+... build
+... eggs
+... parts
+... bin
+... var
+... sdist
+... develop-eggs
+... .installed.cfg
+... lib
+... lib64
+... __pycache__
+... 
+... # Installer logs
+... pip-log.txt
+... 
+... # Unit test / coverage reports
+... .coverage
+... .tox
+... nosetests.xml
+... 
+... # Translations
+... *.mo
+... 
+... # Mr Developer
+... .mr.developer.cfg
+... .project
+... .pydevproject
+... 
+venv
+app.db
