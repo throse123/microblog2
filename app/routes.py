1e17c2c4cca3b5c46b3f8c22b2ad11dafe482dfb
throse123 <throse@packer.edu>
2023-02-16 11:19:31 -0500

    chapter 4

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+import random
 
 @app.route('/')
 @app.route('/index')
@@ -57,3 +58,14 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@ app.route('/quotes')
+def quotes():
+    quote_storage = {
+  "Motiv": ["You can do it!", "Just believe!", "Never stop!", "You are him.", "Never doubt yourself, ever.", "You are the best player in the game.", "Everybody is counting on you.", "In spite of everything you've done for them, eventually they will hate you.", "Those who doubted you will soon retell how they met you."],
+  "Fun": ["He he ha ha", "Your mother", "Joe mama", "Throw it on a dime", "Do you want to get candy?", "I missed the rage!!!!", "Its genover", "stewie, its not your fault, its not your fault","two L's make a Y, get em up, take it down, amariLLO, LLamar, me LLamo"],
+  "Love" : ["You are my sunshine.", "I'm so glad we are together.", "Always be by my side.", "All that you are is all I ever need.", "Every moment is a good one with you."],
+        }
+    random_quotes = random.choice(list(quote_storage.values()))
+    x = random.choice(random_quotes)
+    return render_template('quotes.html', title= 'quotes')
