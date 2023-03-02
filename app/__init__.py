210e733f1af101a61cccface3ee9c3be41f040e9
throse123 <throse@packer.edu>
2023-03-02 13:26:06 -0500

    Chapter 11

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+boostrap = Bootstrap(app)
 
 from app import routes, models, errors
 
